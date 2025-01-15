a926051bb3727612b6a6dff3dffcf23a6bfd566f
shunsuke_kikuchi <shunsuke_kikuchi@jasmine-s.com>
2025-01-15 10:46:44 +0900

    ヘッダーを追加した

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+</script>
+
+<div>
+	<div class="header">
+		<span>トークルーム１</span>
+		<img class="btn config" src="/icons/setting.png" alt="プロフィール設定ボタン" />
+	</div>
+</div>
+
+<style>
+	.header {
+		justify-content: space-between;
+		background-color: #4bce53;
+		color: white;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		flex-grow: 1;
+		padding: 5px;
+		width: 100%;
+		height: 60px;
+		span {
+			font-weight: bold;
+			font-size: large;
+			padding: 10px;
+			text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.4);
+		}
+	}
+
+	.btn {
+		width: 50px;
+		height: 50px;
+	}
+	/* ここまで追記 */
+</style>
